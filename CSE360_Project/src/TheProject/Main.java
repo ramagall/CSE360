0e607cc13a24bf88476f0d5482754f02da1c9e30
chris <chris@DESKTOP-L9ROK41>
2024-04-07 19:21:21 -0700

    Main.java Line 18 Test Comment push

--- CSE360_Project/src/TheProject/Main.java
+++ CSE360_Project/src/TheProject/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         primaryStage.show();
     }
     
-    // yer
+    // Test Comment ...
 
     public static void main(String[] args) {
         launch(args);
